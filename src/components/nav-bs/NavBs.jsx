6b6abf6923a079317fc7470ac468f635b7c34f41
Agustín Pol <agustinpol1991@hotmail.com>
2021-11-17 16:35:02 -0300

    commit navBar modificaciones

--- src/components/nav-bs/NavBs.jsx
+++ src/components/nav-bs/NavBs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavBs = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-blue bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
     <a className="navbar-brand" href="/#">Camisetas-Shop</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
