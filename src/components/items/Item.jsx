885580c90680f1ef24ce63bbca0bb06b715ed368
Agustín Pol <agustinpol1991@hotmail.com>
2021-12-01 11:25:58 -0300

    Quito algunos items de la card para el proximo desafío

--- src/components/items/Item.jsx
+++ src/components/items/Item.jsx
@@ -6,8 +6,8 @@ const Item = ({ prod }) => {
         <div className="myCard">
             <img src={prod.picture} className="img-camisetas" alt="imagen camiseta" />
             <h3>{prod.name}</h3>
-            <p>Precio: $ {prod.price}</p>
-            <p>Categoría: {prod.category}</p>
+            {/* <p>Precio: $ {prod.price}</p> */}
+            {/* <p>Categoría: {prod.category}</p> */}
             <button style={{margin: "0.5rem"}} className="btn btn-primary">Ver detalle</button>
             <button style={{margin: "0.5rem"}} className="btn btn-primary">Agregar</button>
         </div> 
