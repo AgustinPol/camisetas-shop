1e83f35e885bed44c2481f9da92a5330e23a6816
Agustín Pol <agustinpol1991@hotmail.com>
2021-12-15 18:05:01 -0300

    counter sincronizado y funcionando :)

--- src/components/details/ItemDetail.jsx
+++ src/components/details/ItemDetail.jsx
@@ -1,40 +1,82 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import ItemCount from "../item-count/ItemCount";
+// import { useState } from 'react';
+// import "./detail.css";
+
+// const ItemDetail = ({ item }) => {
+ 
+//     const [count, setCount] = useState(0);
+
+//     const onAdd = (contador) => {
+//         console.log("se agregó un item", contador)
+//         setCount(contador)
+//     }
+//     console.log(count)
+        
+//     return (
+//         <article className="divDetail">
+//             <img className="imgDetail" src={item.picture} alt={item.name} />
+//             <h3>{item.name}</h3>
+//             <h4>Precio: $ {item.price}</h4>
+//             <h5>Categoría: {item.category}</h5>
+//             <h5>Envio gratis en Caba y Gba</h5>
+//             <h6>Elegir talle:</h6>
+//             <select className="btn btn-outline-dark" name="talle" id="talles">
+//             <option value="small">Small</option>
+//             <option value="medium">Medium</option>
+//             <option value="large">Large</option>
+//             <option value="xl">XL</option>
+//             <option value="xxl">XXL</option>
+//             </select><br />
+//             <h5>Cantidad disponible: {item.stockAvailable}</h5>
+//             <h6>Elegir Cantidad:</h6>
+//             {count === 0 ? <ItemCount stock={item.stockAvailable} initial={1} final={onAdd} /> : <Link to="/cart"></Link>}
+//             <Link to="/" style={{margin:"1rem"}} className="btn btn-primary" >Volver al Home</Link>
+//         </article>
+//     )
+// }
+
+// export default ItemDetail;
+
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ItemCount from "../item-count/ItemCount";
 import { useState } from 'react';
 import "./detail.css";
 
-const ItemDetail = ({ item }) => {
- 
+const ItemDetail = ({item}) => {
+    
     const [count, setCount] = useState(0);
 
-    const onAdd = (contador) => {
-        console.log("se agregó un item", contador)
+    const addHandler = (contador)=>{
+        console.log('se agrego un item', contador)
         setCount(contador)
     }
-    console.log(count)
-        
+    
+    console.log(count);
     return (
-        <article className="divDetail">
-            <img className="imgDetail" src={item.picture} alt={item.name} />
-            <h3>{item.name}</h3>
-            <h4>Precio: $ {item.price}</h4>
-            <h5>Categoría: {item.category}</h5>
-            <h5>Envio gratis en Caba y Gba</h5>
-            <h6>Elegir talle:</h6>
-            <select className="btn btn-outline-dark" name="talle" id="talles">
-            <option value="small">Small</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
-            <option value="xl">XL</option>
-            <option value="xxl">XXL</option>
-            </select><br />
-            <h5>Cantidad disponible: {item.stockAvailable}</h5>
-            <h6>Elegir Cantidad:</h6>
-            {count === 0 ? <ItemCount stock={item.stockAvailable} initial={1} final={onAdd} /> : <Link to="/cart"></Link>}
-            <Link to="/" style={{margin:"1rem"}} className="btn btn-primary" >Volver al Home</Link>
-        </article>
-    )
+                <article className="divDetail">
+                    <img className="imgDetail" src={item.picture} alt={item.name} />
+                    <h3>{item.name}</h3>
+                    <h4>Precio: $ {item.price}</h4>
+                    <h5>Categoría: {item.category}</h5>
+                    <h5>Envio gratis en Caba y Gba</h5>
+                    <h6>Elegir talle:</h6>
+                    <select className="btn btn-outline-dark" name="talle" id="talles">
+                    <option value="small">Small</option>
+                    <option value="medium">Medium</option>
+                    <option value="large">Large</option>
+                    <option value="xl">XL</option>
+                    <option value="xxl">XXL</option>
+                    </select><br />
+                    <h5>Cantidad disponible: {item.stockAvailable}</h5>
+                    <h6>Elegir Cantidad:</h6>
+                    {count === 0 ? <ItemCount stock={item.stockAvailable} initial={1} onAdd={addHandler} /> : <Link to="/cart" className="btn btn-primary">Ir al Carrito</Link>}
+                    <Link to="/" style={{margin:"1rem"}} className="btn btn-primary" >Volver al Home</Link>
+                </article>
+            )
 }
 
-export default ItemDetail;
+export default ItemDetail
--- src/components/item-count/ItemCount.jsx
+++ src/components/item-count/ItemCount.jsx
@@ -1,40 +1,98 @@
-import React from 'react';
-import { useState } from 'react';
+// import React from 'react';
+// import { useState } from 'react';
 
-const ItemCount = ({stock, initial, final}) => {
+// const ItemCount = ({stock, initial, final}) => {
 
-const [count, setCount] = useState(initial)
+// const [count, setCount] = useState(initial)
 
-    const increment = () => {
-        if (count < stock) {
-            setCount(count + 1);
-        } else if (count === stock) {
-            console.log("No tenemos más unidades del producto");
-        } 
-    }
+//     const increment = () => {
+//         if (count < stock) {
+//             setCount(count + 1);
+//         } else if (count === stock) {
+//             console.log("No tenemos más unidades del producto");
+//         } 
+//     }
 
-    const decrement = () => {
-        if (count > 0) {
-            setCount(count - 1);
+//     const decrement = () => {
+//         if (count > 0) {
+//             setCount(count - 1);
+//         }
+//     }
+
+//     const onAdd = () => {
+//         if(count > 0){
+//             console.log(`Haz agregado ${count} producto/os al carrito`);
+//         } else {
+//             console.log("Aun no haz agregado productos al carrito");
+//         }
+//     }
+
+//     return (
+//         <div>
+//             <span className="btn btn-outline-secondary">{count}</span><br />
+//             <button style={{margin:4}} onClick={decrement} className="btn btn-outline-dark">-</button>
+//             <button style={{margin:4}} onClick={increment} className="btn btn-outline-dark">+</button><br />
+//             <button style={{marginTop:15}} onClick={onAdd} className="btn btn-success">Agregar al Carrito</button><br />
+//         </div>
+//     )
+// }
+
+// export default ItemCount;
+
+
+import React, {useEffect, useState} from 'react';
+
+const ItemCount = (props) =>{
+        
+        const [count, setCount] = useState(parseInt(props.initial));
+        const [countSuma, setCountSuma] = useState(false);
+
+        console.log(count);
+
+        const addHandler = () => {
+            props.onAdd(count)
         }
-    }
 
-    const onAdd = () => {
-        if(count > 0){
-            console.log(`Haz agregado ${count} producto/os al carrito`);
-        } else {
-            console.log("Aun no haz agregado productos al carrito");
-        }
-    }
+        useEffect (()=>{
+            //setCount(parseInt(props.initial[0])); NO SE PUEDE, PORQUE RECIEN SE MONTA. NO ALCANZA NINGUNA PROPIEDAD.
+            //console.log("App montada!");
+            return;
+        }, []); //Ese effect sólo se ejecutará una vez montado el componente y nada más
 
-    return (
-        <div>
-            <span className="btn btn-outline-secondary">{count}</span><br />
-            <button style={{margin:4}} onClick={decrement} className="btn btn-outline-dark">-</button>
-            <button style={{margin:4}} onClick={increment} className="btn btn-outline-dark">+</button><br />
-            <button style={{marginTop:15}} onClick={onAdd} className="btn btn-success">Agregar al Carrito</button><br />
-        </div>
-    )
-}
+        useEffect(() =>{
+            //console.log ("Hiciste click en +")
+        }, [countSuma]); //Este effect se aplica en cada mount y además cuando se hace click en + (para practicar los effects)
+
+        const removeHandle = () =>{
+            if (count >=1)
+                setCount(count - 1 );
+        };
+
+        const addHandle = () => {
+            if (count < props.stock){
+                setCount (count + 1);
+                setCountSuma(!countSuma);
+            }
+        };
+        //Sólo agregará el addOn (funcion del padre) en caso que el count sea mayor a cero, sino no.
+        return(
+            <div>
+                <div className="d-flex justify-content-around">
+                    <button className="btn btn btn-outline-primary btn-sm" onClick={removeHandle}>-</button>
+                    <h5 className="card-text">{count}</h5>
+                    <button className="btn btn btn-outline-primary btn-sm" onClick={addHandle}>+</button>
+                </div>
+                <p className="card-text"></p>
+
+                {count >0 ? 
+                    <button className="btn btn btn-outline-primary btn-sm" onClick={addHandler}>
+                        Agregar al carrito
+                    </button> :
+                    null
+                }
+                
+            </div>    
+        )
+    }
 
 export default ItemCount;
